4a123ad89c85c1b0a9db4723d3b2be1d7f4984a6
Rose <rsekhon32@my.bcit.ca>
2021-11-21 01:48:01 -0800

    clearIndex

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 /* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+
+/* INDEX PAGE */
+function cleanUpIndex(){
+    const element = document.querySelectorAll(".contact")
+    for (let contact of element){
+        contact.remove()
+    }
+    
+    
+}
